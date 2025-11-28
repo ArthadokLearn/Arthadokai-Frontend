13a75c0a531a506b61be703a5e7ce52dab9b797e
DagonMastema <venkat.sai.sandeep@gmail.com>
2025-11-28 10:10:32 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,19 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { StrictMode } from "react";
+import "./index.css";
+import { MathJaxContext } from "better-react-mathjax";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+const config = {
+  tex: {
+    inlineMath: [["$", "$"], ["\\(", "\\)"]],
+    displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+  },
+};
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
+    <MathJaxContext config={config}>
+      <App />
+    </MathJaxContext>
+  </StrictMode>
+);
